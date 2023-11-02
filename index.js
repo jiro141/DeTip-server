d71388d2dd591fbf4b8f7bf46fe8e0e4c8fc9530
jiro141 <91983449+jiro141@users.noreply.github.com>
2023-11-02 14:34:42 -0400

    cambio

--- index.js
+++ index.js
@@ -11,27 +11,43 @@ app.use(express.json());
 // Ruta para enviar un correo electrónico
 app.post('/enviar-correo', async (req, res) => {
   const { destinatario, asunto, mensaje, nombreApellido, nombreEmpresa, cargo, fecha, hora } = req.body;
-  // Agrega registros para mostrar los valores de los datos
-  // console.log('Destinatario:', destinatario);
+  const responseData = `
+    <html>
+      <body>
+        <h1>Información recibida:</h1>
+        <p>Destinatario: ${destinatario}</p>
+        <p>Asunto: ${asunto}</p>
+        <p>Mensaje: ${mensaje}</p>
+        <p>Nombre y Apellido: ${nombreApellido}</p>
+        <p>Nombre de la Empresa: ${nombreEmpresa}</p>
+        <p>Cargo: ${cargo}</p>
+        <p>Fecha: ${fecha}</p>
+        <p>Hora: ${hora}</p>
+      </body>
+    </html>
+  `;
 
-  try {
-    // Llama a la función enviarCorreo con los datos del formulario
-    await emailController.enviarCorreo(
-      destinatario,
-      asunto,
-      mensaje,
-      nombreApellido,
-      nombreEmpresa,
-      cargo,
-      fecha,
-      hora
-    );
+  res.send(responseData); // Enviar la respuesta al navegador
+});
 
-    res.status(200).json({ message: 'Correo enviado con éxito' });
-  } catch (error) {
-    console.error('Error al enviar el correo electrónico:', error);
-    res.status(500).json({ message: 'Error al enviar el correo electrónico' });
-  }
+try {
+  // Llama a la función enviarCorreo con los datos del formulario
+  await emailController.enviarCorreo(
+    destinatario,
+    asunto,
+    mensaje,
+    nombreApellido,
+    nombreEmpresa,
+    cargo,
+    fecha,
+    hora
+  );
+
+  res.status(200).json({ message: 'Correo enviado con éxito' });
+} catch (error) {
+  console.error('Error al enviar el correo electrónico:', error);
+  res.status(500).json({ message: 'Error al enviar el correo electrónico' });
+}
 });
 
 app.listen(port, () => {
