6a6c815ec35698ad5eb142645fd8c17a37f34195
jiro141 <91983449+jiro141@users.noreply.github.com>
2023-11-02 11:03:21 -0400

    first commit

---
+++ emailController.js
@@ -0,0 +1,58 @@
+const nodemailer = require('nodemailer');
+const fs = require('fs');
+// Configura el transporte para enviar correos electrónicos
+const transporter = nodemailer.createTransport({
+    service: 'Gmail', // Elige el servicio de correo que desees usar (por ejemplo, Gmail)
+    auth: {
+        user: 'detipventas@gmail.com', // Coloca tu dirección de correo electrónico
+        pass: 'bdtf ahbz xyga hbgn', // Coloca tu contraseña
+    },
+});
+
+// Función para enviar un correo electrónico
+const enviarCorreo = async (destinatario, asunto, mensaje, nombreApellido, nombreEmpresa, cargo, fecha, hora) => {
+    try {
+        const templateHtml = fs.readFileSync('./template.html', 'utf-8');
+        const cuerpoCorreo = templateHtml
+            .replace('{nombreApellido}', nombreApellido)
+            .replace('{nombreEmpresa}', nombreEmpresa)
+            .replace('{cargo}', cargo)
+            .replace('{fecha}', fecha)
+            .replace('{hora}', hora);
+        // Configura los detalles del correo electrónico
+        const mailOptionsUsuario = {
+            from: 'detipventas@gmail.com', // Remitente
+            to: destinatario, // Destinatario
+            subject: '¡Gracias por comunicarte con nosotros!', // Asunto del correo
+            text: mensaje, // Cuerpo del correo
+            // Agrega los campos adicionales en el cuerpo del correo en formato HTML
+            html: cuerpoCorreo,
+        };
+        // Envía el correo electrónico
+        await transporter.sendMail(mailOptionsUsuario);
+        const correo = fs.readFileSync('./mensaje.html', 'utf-8');
+        const cuerpCorreo = correo
+            .replace('{nombreApellido}', nombreApellido)
+            .replace('{nombreEmpresa}', nombreEmpresa)
+            .replace('{cargo}', cargo)
+            .replace('{fecha}', fecha)
+            .replace('{mensaje}',mensaje)
+            .replace('{hora}', hora);
+        const mailOptionsAdmin = {
+            from: destinatario, // Remitente (tu correo)
+            to: 'detipventas@gmail.com', // Tu propia dirección de correo
+            subject: 'Nueva solicitud', // Asunto del correo
+            text: 'Datos del usuario:', // Cuerpo del correo
+            html: cuerpCorreo
+        };
+
+        // Envía el correo electrónico a ti mismo
+        await transporter.sendMail(mailOptionsAdmin);
+
+        console.log('Correo electrónico enviado con éxito');
+    } catch (error) {
+        console.error('Error al enviar el correo electrónico:', error);
+    }
+};
+
+module.exports = { enviarCorreo };
---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = 5000; // Puedes usar cualquier número de puerto que prefieras
+
+app.use(cors());
+const emailController = require('./emailController'); // Importa el controlador de envío de correos
+
+app.use(express.json());
+
+// Ruta para enviar un correo electrónico
+app.post('/enviar-correo', async (req, res) => {
+  const { destinatario, asunto, mensaje, nombreApellido, nombreEmpresa, cargo, fecha, hora } = req.body;
+  // Agrega registros para mostrar los valores de los datos
+  // console.log('Destinatario:', destinatario);
+
+  try {
+    // Llama a la función enviarCorreo con los datos del formulario
+    await emailController.enviarCorreo(
+      destinatario,
+      asunto,
+      mensaje,
+      nombreApellido,
+      nombreEmpresa,
+      cargo,
+      fecha,
+      hora
+    );
+
+    res.status(200).json({ message: 'Correo enviado con éxito' });
+  } catch (error) {
+    console.error('Error al enviar el correo electrónico:', error);
+    res.status(500).json({ message: 'Error al enviar el correo electrónico' });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Servidor Express escuchando en el puerto ${port}`);
+});
