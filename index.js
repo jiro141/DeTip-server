f0e179c77e3693e7f0deb680da54041a012152aa
jiro141 <91983449+jiro141@users.noreply.github.com>
2023-11-03 10:05:31 -0400

    cambio

--- index.js
+++ index.js
@@ -26,29 +26,28 @@ app.post('/enviar-correo', async (req, res) => {
       </body>
     </html>
   `;
-
   res.send(responseData); // Enviar la respuesta al navegador
+  try {
+    // Llama a la función enviarCorreo con los datos del formulario
+    await emailController.enviarCorreo(
+      destinatario,
+      asunto,
+      mensaje,
+      nombreApellido,
+      nombreEmpresa,
+      cargo,
+      fecha,
+      hora
+    );
+
+    res.status(200).json({ message: 'Correo enviado con éxito' });
+  } catch (error) {
+    console.error('Error al enviar el correo electrónico:', error);
+    res.status(500).json({ message: 'Error al enviar el correo electrónico' });
+  }
 });
 
-try {
-  // Llama a la función enviarCorreo con los datos del formulario
-  await emailController.enviarCorreo(
-    destinatario,
-    asunto,
-    mensaje,
-    nombreApellido,
-    nombreEmpresa,
-    cargo,
-    fecha,
-    hora
-  );
 
-  res.status(200).json({ message: 'Correo enviado con éxito' });
-} catch (error) {
-  console.error('Error al enviar el correo electrónico:', error);
-  res.status(500).json({ message: 'Error al enviar el correo electrónico' });
-}
-});
 
 app.listen(port, () => {
   console.log(`Servidor Express escuchando en el puerto ${port}`);
